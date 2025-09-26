e7bb012bad36dde353e50b40e9bcb10959cc81f4
yash691333 <131446626+yash691333@users.noreply.github.com>
2025-09-26 14:57:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_vpc" "default" {
   default = true
 }
 
-# Get all subnets in the VPC
+# Get all subnets in that VPC
 data "aws_subnets" "all" {
   filter {
     name   = "vpc-id"
@@ -11,38 +11,38 @@ data "aws_subnets" "all" {
   }
 }
 
-# Look up details for each subnet
+# Look up each subnet's details
 data "aws_subnet" "details" {
   for_each = toset(data.aws_subnets.all.ids)
   id       = each.value
 }
 
-# Keep only subnets in supported AZs (exclude us-east-1e)
+# Only keep subnets in supported AZs (exclude us-east-1e)
 locals {
-  eks_subnets = [
+  eks_subnet_ids = [
     for s in data.aws_subnet.details :
     s.id if contains(["us-east-1a", "us-east-1b", "us-east-1c"], s.availability_zone)
   ]
 }
 
-# Use filtered subnets for EKS control plane
+# Use filtered subnets for control plane
 resource "aws_eks_cluster" "my_cluster" {
   name     = "my-cluster"
   role_arn = aws_iam_role.cbz_eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = local.eks_subnets
+    subnet_ids = local.eks_subnet_ids
   }
 
   depends_on = [aws_iam_role_policy_attachment.cbz_eks_cluster_role_policy]
 }
 
-# Worker node group (can use the same filtered subnets)
+# Use same subnets for node group (you can expand later)
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.my_cluster.name
   node_group_name = "example-node-group"
   node_role_arn   = aws_iam_role.cbz_eks_node_group_role.arn
-  subnet_ids      = local.eks_subnets
+  subnet_ids      = local.eks_subnet_ids
 
   scaling_config {
     desired_size = 2
