d886c3eec2c59463fcb6b8ab82a2d357f20d7f08
yash691333 <131446626+yash691333@users.noreply.github.com>
2025-09-26 14:53:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,100 +1,56 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 # Get default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-# Get subnets but exclude unsupported AZ (us-east-1e)
-data "aws_subnets" "supported" {
+# Get all subnets in the VPC
+data "aws_subnets" "all" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
 }
 
-# Filter subnets by supported AZs
-data "aws_subnet" "filtered" {
-  for_each = toset(data.aws_subnets.supported.ids)
-
-  id = each.value
+# Look up details for each subnet
+data "aws_subnet" "details" {
+  for_each = toset(data.aws_subnets.all.ids)
+  id       = each.value
 }
 
+# Keep only subnets in supported AZs (exclude us-east-1e)
 locals {
-  # Keep only subnets in supported AZs
-  eks_subnet_ids = [
-    for s in data.aws_subnet.filtered :
+  eks_subnets = [
+    for s in data.aws_subnet.details :
     s.id if contains(["us-east-1a", "us-east-1b", "us-east-1c"], s.availability_zone)
   ]
 }
 
-# IAM role for EKS cluster
-resource "aws_iam_role" "cbz_eks_cluster_role" {
-  name = "cbz-eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cbz_eks_cluster_role_policy" {
-  role       = aws_iam_role.cbz_eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-# Create EKS Cluster
+# Use filtered subnets for EKS control plane
 resource "aws_eks_cluster" "my_cluster" {
   name     = "my-cluster"
   role_arn = aws_iam_role.cbz_eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = local.eks_subnet_ids
+    subnet_ids = local.eks_subnets
   }
 
   depends_on = [aws_iam_role_policy_attachment.cbz_eks_cluster_role_policy]
 }
 
-# IAM role for worker nodes
-resource "aws_iam_role" "cbz_eks_node_role" {
-  name = "cbz-eks-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cbz_eks_node_role_policy" {
-  role       = aws_iam_role.cbz_eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
-
-# Node group
-resource "aws_eks_node_group" "my_node_group" {
+# Worker node group (can use the same filtered subnets)
+resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.my_cluster.name
-  node_group_name = "my-node-group"
-  node_role_arn   = aws_iam_role.cbz_eks_node_role.arn
-  subnet_ids      = local.eks_subnet_ids
+  node_group_name = "example-node-group"
+  node_role_arn   = aws_iam_role.cbz_eks_node_group_role.arn
+  subnet_ids      = local.eks_subnets
+
   scaling_config {
     desired_size = 2
     max_size     = 3
     min_size     = 1
   }
+
+  instance_types = ["t2.medium"]
 
   depends_on = [aws_eks_cluster.my_cluster]
 }
